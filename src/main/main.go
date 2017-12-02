dc5d24c5d7a3f418038ce29669a245cf071bdc6a
qiyazhong <qiyazhong@163.com>
2017-12-02 23:56:47 +0800

    channel example

--- src/main/main.go
+++ src/main/main.go
@@ -148,3 +148,29 @@ func testChannel() {
 		fmt.Println("Timer 2 stopped")
 	}
 }
+
+func worker(id int, jobs <-chan int, result chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "processing job", j)
+		time.Sleep(time.Second)
+		result <- j * 2
+	}
+}
+
+func testChannel2() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+	//启动三个worker
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+	//循环9次，发送任务
+	for j := 1; j <= 9; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	//循环得到结果
+	for a := 1; a <= 9; a++ {
+		<-results
+	}
+}
